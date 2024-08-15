66a3a2235289ff7b0e7cf8bac4ad80ab72ffb4e6
Pavel Dubrova <pashadubrova@gmail.com>
2024-08-15 03:48:30 +0300

    Android.mk: Update path for audio-hal-ar
    It so happened that we have a new Audioreach based HAL and legacy
    one (pre SM8450 SoC) at the same time. The new HAL has the same
    path as the old one except for the -ar suffix. Update all paths
    accordingly.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- Android.mk
+++ Android.mk
@@ -22,8 +22,8 @@ LOCAL_C_INCLUDES += \
     external/expat/lib \
     vendor/qcom/opensource/core-utils/fwk-detect \
     vendor/qcom/opensource/pal \
-    vendor/qcom/opensource/audio-hal/primary-hal/hal/audio_extn \
-    vendor/qcom/opensource/audio-hal/primary-hal/hal
+    vendor/qcom/opensource/audio-hal/primary-hal-ar/hal/audio_extn \
+    vendor/qcom/opensource/audio-hal/primary-hal-ar/hal
 
 LOCAL_SRC_FILES := \
     SoundTriggerDevice.cpp \
